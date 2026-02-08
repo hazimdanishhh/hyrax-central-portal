777b854bcec2d8f14b1829d214fa541a97fcf8e7
Hazim <hazimdanishhh@gmail.com>
2026-02-08 21:47:27 +0800

    Fix some styling issues

--- src/pages/user/employees/employeeProfile/EmployeeProfile.jsx
+++ src/pages/user/employees/employeeProfile/EmployeeProfile.jsx
@@ -20,6 +20,7 @@ import StatusBadge from "../../../../components/status/statusBadge/StatusBadge";
 import SectionHeader from "../../../../components/sectionHeader/SectionHeader";
 import RouterButton from "../../../../components/buttons/routerButton/RouterButton";
 import Breadcrumbs from "../../../../components/breadcrumbs/Breadcrumbs";
+import CardWrapper from "../../../../components/cardWrapper/CardWrapper";
 
 export default function EmployeeProfile() {
   const [message, setMessage] = useState({ text: "", type: "" });
@@ -47,138 +48,154 @@ export default function EmployeeProfile() {
               icon1={UsersFour}
               current={`${employee.preferred_name}'s Profile`}
               to1="/app/employees"
-              name1="EMPLOYEES"
+              name1="Employees"
             />
-            <CardSection>
-              <div className="profileOverview">
-                <div className="profilePhoto">
-                  <img
-                    src={employee.avatar_url || "/profilePhoto/default.webp"}
-                    alt={employee.full_name || "No Name"}
-                  />
-                </div>
-
-                <div className="profileOverviewDetails">
-                  <p className="textBold textM">
-                    {employee.full_name || "No Name"}
-                    <span className="textRegular textXS">
-                      ({employee.preferred_name || "No Name"})
-                    </span>
-                  </p>
-                  <p className="textLight textXXS">
-                    {employee.department_name || "No Department Set"}
-                  </p>
-                  <p className="textLight textXXS">
-                    {employee.position || "No Position Set"}
-                  </p>
-                  <StatusBadge status={employee.employment_status_name} />
-                </div>
-              </div>
-            </CardSection>
-
-            <CardSection>
-              <div className="profileSectionHeader">
-                <h2 className="textBold textS">Personal Information</h2>
-              </div>
-
-              <CardLayout style="cardLayout3">
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">Department</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.department_name || "null"}
-                  </p>
-                </div>
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">Position</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.position || "null"}
-                  </p>
-                </div>
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">Email (Work)</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.email_work || "null"}
-                  </p>
-                </div>
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">Phone (Work)</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.phone_work || "null"}
-                  </p>
-                </div>
-
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">Employee ID</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.employee_id || "null"}
-                  </p>
-                </div>
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">System ID</strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.profile_id || "null"}
-                  </p>
-                </div>
-              </CardLayout>
+            <CardWrapper>
               <CardLayout style="cardLayout1">
-                <div className="profileDetails">
-                  <strong className="profileLabel textXXS">
-                    Address (Work)
-                  </strong>
-                  <p className="profileData textRegular textXXS">
-                    {employee.address_work || "null"}
-                  </p>
-                </div>
+                <CardSection>
+                  <div className="profileOverview">
+                    <div className="profilePhoto">
+                      <img
+                        src={
+                          employee.avatar_url || "/profilePhoto/default.webp"
+                        }
+                        alt={employee.full_name || "No Name"}
+                      />
+                    </div>
+
+                    <div className="profileOverviewDetails">
+                      <p className="textBold textM">
+                        {employee.full_name || "No Name"}
+                        <span className="textRegular textXS">
+                          ({employee.preferred_name || "No Name"})
+                        </span>
+                      </p>
+                      <p className="textLight textXXS">
+                        {employee.department_name || "No Department Set"}
+                      </p>
+                      <p className="textLight textXXS">
+                        {employee.position || "No Position Set"}
+                      </p>
+                      <StatusBadge status={employee.employment_status_name} />
+                    </div>
+                  </div>
+                </CardSection>
+
+                <CardSection>
+                  <div className="profileSectionHeader">
+                    <h2 className="textBold textS">Personal Information</h2>
+                  </div>
+
+                  <CardLayout style="cardLayout3">
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        Department
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.department_name || "null"}
+                      </p>
+                    </div>
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">Position</strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.position || "null"}
+                      </p>
+                    </div>
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        Email (Work)
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.email_work || "null"}
+                      </p>
+                    </div>
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        Phone (Work)
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.phone_work || "null"}
+                      </p>
+                    </div>
+
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        Employee ID
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.employee_id || "null"}
+                      </p>
+                    </div>
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        System ID
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.profile_id || "null"}
+                      </p>
+                    </div>
+                  </CardLayout>
+                  <CardLayout style="cardLayout1">
+                    <div className="profileDetails">
+                      <strong className="profileLabel textXXS">
+                        Address (Work)
+                      </strong>
+                      <p className="profileData textRegular textXXS">
+                        {employee.address_work || "null"}
+                      </p>
+                    </div>
+                  </CardLayout>
+                </CardSection>
+
+                {employee.manager_name && (
+                  <CardSection>
+                    <div className="departmentHeader">
+                      <h2 className="textRegular textL">
+                        {employee.preferred_name}'s Reporting Manager
+                      </h2>
+                    </div>
+                    <div className="profileOverview">
+                      <div className="profilePhoto">
+                        <img
+                          src={
+                            employee.manager_avatar_url ||
+                            "/profilePhoto/default.webp"
+                          }
+                          alt={employee.manager_name}
+                        />
+                      </div>
+
+                      <div className="profileOverviewDetails">
+                        <p className="textBold textM">
+                          {employee.manager_name}{" "}
+                          <span className="textRegular textXS">
+                            ({employee.manager_preferred_name})
+                          </span>
+                        </p>
+                        <StatusBadge
+                          status={employee.manager_employment_status_name}
+                        />
+                        <p className="textLight textXXS">
+                          <IdentificationBadge /> {employee.manager_employee_id}
+                        </p>
+                        <p className="textLight textXXS">
+                          <UsersThree /> {employee.manager_department_name}
+                        </p>
+                        <p className="textLight textXXS">
+                          <UserCircle /> {employee.manager_position}
+                        </p>
+                        <p className="textLight textXXS">
+                          <Envelope /> {employee.manager_email}
+                        </p>
+                        <p className="textLight textXXS">
+                          <Phone /> {employee.manager_phone}
+                        </p>
+                      </div>
+                    </div>
+                  </CardSection>
+                )}
               </CardLayout>
-            </CardSection>
-
-            {employee.manager_name && (
-              <CardSection>
-                <div className="departmentHeader">
-                  <h2 className="textRegular textL">
-                    {employee.preferred_name}'s Reporting Manager
-                  </h2>
-                </div>
-                <div className="profileOverview">
-                  <div className="profilePhoto">
-                    <img
-                      src={
-                        employee.manager_avatar_url ||
-                        "/profilePhoto/default.webp"
-                      }
-                      alt={employee.manager_name}
-                    />
-                  </div>
-
-                  <div className="profileOverviewDetails">
-                    <p className="textBold textM">
-                      {employee.manager_name}{" "}
-                      <span className="textRegular textXS">
-                        ({employee.manager_preferred_name})
-                      </span>
-                    </p>
-                    <StatusBadge
-                      status={employee.manager_employment_status_name}
-                    />
-                    <p className="textLight textXXS">
-                      <IdentificationBadge /> {employee.manager_employee_id}
-                    </p>
-                    <p className="textLight textXXS">
-                      <UsersThree /> {employee.manager_department_name}
-                    </p>
-                    <p className="textLight textXXS">
-                      <UserCircle /> {employee.manager_position}
-                    </p>
-                    <p className="textLight textXXS">
-                      <Envelope /> {employee.manager_email}
-                    </p>
-                    <p className="textLight textXXS">
-                      <Phone /> {employee.manager_phone}
-                    </p>
-                  </div>
-                </div>
-              </CardSection>
-            )}
+            </CardWrapper>
           </div>
         </div>
       </section>
