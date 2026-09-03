6fa540ed67b759ac0236290dd4fd07ffd8ae43db
Hazim <hazimdanishhh@gmail.com>
2026-09-03 13:02:57 +0800

    refactor: user list

--- src/components/users/userList/UserList.jsx
+++ src/components/users/userList/UserList.jsx
@@ -12,28 +12,36 @@ export default function UserList({ user, onClick, saving, deleting }) {
       whileHover={{ y: -3 }}
     >
       <div className="employeeListHeaderContainer">
-        <div className="employeeCardPhoto">
-          <img
-            src={user.avatar_url || "/profilePhoto/default.webp"}
-            alt={user.full_name}
-          />
-        </div>
-
-        <div className="employeeCardHeaderDetails">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.4rem",
+          }}
+        >
+          <div className="employeeCardPhoto">
+            <img
+              src={user.avatar_url || "/profilePhoto/default.webp"}
+              alt={user.full_name}
+            />
+          </div>
           <p className="textBold textXXS">{user.full_name}</p>
           <StatusBox status={user.department?.sub} type="dark" />
-          <StatusBox
-            status={user.role?.name}
-            type={
-              user.role?.name === "staff"
-                ? "green"
-                : user.role?.name === "manager"
-                  ? "blue"
-                  : "yellow"
-            }
-          />
         </div>
         <div className="employeeCardStatusContainer">
+          <div className="employeeCardHeaderDetails">
+            <StatusBox
+              status={user.role?.name}
+              type={
+                user.role?.name === "staff"
+                  ? "green"
+                  : user.role?.name === "manager"
+                    ? "blue"
+                    : "yellow"
+              }
+            />
+          </div>
+
           <button className="listArrow iconButton2">
             <NotePencilIcon size={16} weight="light" />
           </button>
